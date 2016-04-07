58f3f7e09343e1f199a9b0dacc8dbcd9fb38bb99
PGEsteves <paulo.esteves17@gmail.com>
2016-04-07 16:07:34 +0200

    inv

--- Game2/Inventory.cs
+++ Game2/Inventory.cs
@@ -14,19 +14,16 @@ namespace Game2
         ContentManager Content;
         List<Item> allItems;
         Vector2 position;
-        double multiplierX = 0, multiplierY = 0;
+        Item isClickedItem;
+        public Boolean IsDone=true;
 
         private List<Item> LoadItems()
         {
             List<Item> items = new List<Item>();
-
-            string path = (System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments))+ "\\MayhemOfSound\\SaveData\\User\\Items.txt";
+            string path = (System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments)) + "\\MayhemOfSound\\SaveData\\User\\Items.txt";
 
             StreamReader listItems = new StreamReader(path);
-            string allI = listItems.ReadLine();          
-          
-
-
+            string allI = listItems.ReadLine();
 
 
             listItems.Close();
@@ -48,10 +45,7 @@ namespace Game2
                 }
             }
 
-            return (items);                    
-            
-
-
+            return (items);
 
         }
 
@@ -63,64 +57,72 @@ namespace Game2
             {
                 item.img = Content.Load<Texture2D>("Items\\" + item.id);
             }
-
             position.X = (float)0.03;
-            position.Y = (float)0.03;
-
+            position.Y = (float)0.04;
 
         }
         public void UnloadContent()
         {
         }
 
-        public void Update(GameTime gameTime)
-
-        {            
-
-
+        public void Update(GameTime gameTime, string saveName)
+        {
+            foreach (var item in allItems)
+            {
+                item.IsClicked();
+                if (item.clicked)
+                {
+                    isClickedItem = item;
+                }
+            }
+        
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            int c = allItems.Count;
-            /*foreach (var item in allItems)
-            {
-                
-                spriteBatch.Draw(item.img, new Vector2((float)((spriteBatch.GraphicsDevice.Viewport.Width *( position.X + multiplierX))), (float)(spriteBatch.GraphicsDevice.Viewport.Height * (position.Y + multiplierY))), Color.White);
-                multiplierX = multiplierX == 0.12*(7) ? 0 : multiplierX += 0.12;
-                multiplierY = multiplierX == 0.12*(7) ? multiplierY += 0.16 : multiplierY;
-                multiplierY = multiplierY == 0.16*(2) ? 0 : multiplierY;
-                         
-            }*/
 
 
 
-            spriteBatch.Draw(Content.Load<Texture2D>("Items\\bgInv"), Vector2.Zero,null,Color.White,0,Vector2.Zero,0.63f,SpriteEffects.None,0);
 
+            spriteBatch.Draw(Content.Load<Texture2D>("Items\\bgInv"),(new Rectangle (0,0,(int)(spriteBatch.GraphicsDevice.Viewport.Width*0.64),(int)(spriteBatch.GraphicsDevice.Viewport.Height*0.55))),Color.White);
+            //spriteBatch.Draw(Content.Load<Texture2D>("Items\\bgInv"), new Rectangle(),null,Color.White,0,Vector2.Zero,0.80f,SpriteEffects.None,0);
+
+            spriteBatch.Draw(Content.Load<Texture2D>("Items\\bgInv"),
+                (new Rectangle(((int)(spriteBatch.GraphicsDevice.Viewport.Width * 0.64)),0
+                ,(int)(spriteBatch.GraphicsDevice.Viewport.Width * 0.36), (int)(spriteBatch.GraphicsDevice.Viewport.Height * 0.55))), Color.White);
+            spriteBatch.Draw(Content.Load<Texture2D>("Items\\bgInv"),
+                (new Rectangle(0, ((int)(spriteBatch.GraphicsDevice.Viewport.Height * 0.55))
+                , (int)(spriteBatch.GraphicsDevice.Viewport.Width * 1), (int)(spriteBatch.GraphicsDevice.Viewport.Height * 0.45))), Color.White);
             for (int y = 0; y < 5; y++)
             {
                 for (int x = 0; x < 10; x++)
                 {
-
-                    if ((allItems.Count - 1) < (x + y * 10))
-
+                    if ((allItems.Count - 1) >= (x + y * 10))
                     {
-                        //preecher com blank
-                        spriteBatch.Draw(Content.Load<Texture2D>("Items\\0"), new Vector2((float)((spriteBatch.GraphicsDevice.Viewport.Width * (position.X + (x * 0.06)))), (float)(spriteBatch.GraphicsDevice.Viewport.Height * (position.Y + (y * 0.16)))), Color.White);
+                        spriteBatch.DrawString(Content.Load<SpriteFont>("angryblue"),allItems[x+y*10].clicked.ToString(),Vector2.Zero,Color.White);
+                    }
+                    
+
+                    if (((allItems.Count - 1) >= (x + y * 10)) && (isClickedItem==allItems[x+y*10]))
+                    {
+
+                        spriteBatch.Draw(Content.Load<Texture2D>("Items\\esc"), allItems[x+y*10].position, null, Color.White, 0, Vector2.Zero, 1.1f, SpriteEffects.None, 0);
+                       
                     }
                     else
                     {
-
-                        spriteBatch.Draw(allItems[x + y * 10].img, new Vector2((float)((spriteBatch.GraphicsDevice.Viewport.Width * (position.X + (x * 0.06)))), (float)(spriteBatch.GraphicsDevice.Viewport.Height * (position.Y + (y * 0.16)))), Color.White);
-                       // spriteBatch
-
+                        spriteBatch.Draw(Content.Load<Texture2D>("Items\\vazio"), new Vector2((float)((spriteBatch.GraphicsDevice.Viewport.Width * ((position.X - 0.002) + (x * 0.06)))), (float)(spriteBatch.GraphicsDevice.Viewport.Height * ((position.Y - 0.003) + (y * 0.1)))), null, Color.White, 0, Vector2.Zero, 1.1f, SpriteEffects.None, 0);
                     }
-                    //multiplierX = multiplierX == 0.06 * (allItems.Count) ? 0 : multiplierX += 0.06;
-                    //multiplierY = multiplierX == 0.06 * (allItems.Count) ? multiplierY += 0.16 : multiplierY;
-                    //multiplierY = multiplierY == 0.16 * (2) ? 0 : multiplierY;
+                    
 
-
-
+                    if ((allItems.Count - 1) >= (x + y * 10))
+                        {
+                            spriteBatch.Draw(allItems[x + y * 10].img, new Vector2((float)((spriteBatch.GraphicsDevice.Viewport.Width * (position.X + (x * 0.06)))), (float)(spriteBatch.GraphicsDevice.Viewport.Height * (position.Y + (y * 0.1)))), Color.White);
+                            allItems[x + y * 10].position = new Vector2((float)((spriteBatch.GraphicsDevice.Viewport.Width * ((position.X - 0.002) + (x * 0.06)))), (float)(spriteBatch.GraphicsDevice.Viewport.Height * ((position.Y - 0.003) + (y * 0.1))));
+                            // spriteBatch                       
+                        }
+                    
+                    
                 }
             }
 
--- Game2/Item.cs
+++ Game2/Item.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
+
 
 namespace Game2
 {
@@ -12,11 +14,33 @@ namespace Game2
         public Texture2D img { get; set; }
         public string id { get; set; }
         public string description { get; set; }
+        public Vector2 position;
+        public Boolean clicked { get; private set; } = false;
 
         public Item (string id)
         {
             this.id = id;
         }
-               
+
+        private bool IsCursorOn()
+        {
+            MouseState cursor = Mouse.GetState();
+
+            if (position.X < cursor.X && position.Y < cursor.Y && position.X + img.Width > cursor.X && position.Y+ img.Height > cursor.Y)
+                return true;
+            return false;
+        }
+
+        public void IsClicked()
+        {
+            MouseState cursor = Mouse.GetState();
+            if (IsCursorOn() && (cursor.LeftButton == ButtonState.Pressed))
+                clicked = true;
+            else
+                clicked = false;
+
+            
+        }
+
     }
 }
